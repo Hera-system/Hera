56475c27f24a8d50ebe6cd9250a405a74fcc1cab
OldTyT <nezlobin118@gmail.com>
2022-12-15 20:36:50 +0800

    Added page on commads

--- app/routes.py
+++ app/routes.py
@@ -130,7 +130,7 @@ def templates():
 @login_required
 def commands():
     if current_user.is_authenticated:
-        page = request.args.get('next', default="page", type=int)
+        page = request.args.get('next', default=1, type=int)
         # command_exec = reversed(CommandExecution.query.all())
         command_exec = reversed(CommandExecution.query.paginate(page, 50, True))
         return render_template("commands.html", commands=command_exec)
